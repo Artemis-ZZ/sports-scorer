f93cb696d846e983b67700db0b47fc5b800d6f40
Artemis-ZZ <Jiawen1325@gmail.com>
2026-06-28 22:15:37 +0800

    Initial: 垒球+户外运动成绩管理系统 PWA
    - 8 tabs: 学生/考勤/身体素质/专项/赛事/阳光长跑/查询/总览
    - 垒球: 专项一20分 + 专项二20分
    - 户外运动: 专项一30分 + 专项二10分
    - 赛事: 竞赛一/二(各5分) + 校级赛事(10分)，上限10分
    - 41列 Excel 导出匹配中传体育部计分册模板

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'sports-v1';
+
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(['.', 'index.html', 'manifest.json']);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)));
+    }).then(function() { return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).then(function(response) {
+      if (response && response.status === 200) {
+        const clone = response.clone();
+        caches.open(CACHE).then(function(cache) { cache.put(e.request, clone); });
+      }
+      return response;
+    }).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});
